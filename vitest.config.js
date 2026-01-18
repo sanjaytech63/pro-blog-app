88e6bc2f0d6a66725f08d324fc878ff7a5deb7dc
sanjay <sanjaychoudhary.devoy@gmail.com>
2026-01-18 21:30:56 +0530

    normalize line endings

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'jsdom',
-    }
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+  },
 })
