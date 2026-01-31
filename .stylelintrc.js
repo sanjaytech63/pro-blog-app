d2c86383d7187d756fc4e3f6755e54169c4e1a35
Sanjay Choudhary <sanjaytech6375@gmail.com>
2026-01-31 14:54:06 +0530

    fix type bug

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  rules: {
+    /* Tailwind / modern CSS */
+    'at-rule-no-unknown': null,
+    'at-rule-empty-line-before': null,
+
+    /* OKLCH support */
+    'color-function-notation': null,
+    'lightness-notation': null,
+    'hue-degree-notation': null,
+
+    /* CSS variables & Tailwind layers */
+    'custom-property-empty-line-before': null,
+  },
 }
