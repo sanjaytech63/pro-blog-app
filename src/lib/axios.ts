d07e2cbdbf26b8e087ce21e60958d1a83277203a
Sanjay Choudhary <sanjaytech6375@gmail.com>
2026-04-06 21:29:53 +0530

    fix api base url in axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Use relative URLs by default (same domain in prod)
 // Only use absolute URL if environment variable explicitly set for a different domain
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: '',
   withCredentials: true,
   timeout: 15000,
 })
