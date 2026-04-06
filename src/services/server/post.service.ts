218294b13a41bebea8a260691a21f5db177b0a80
Sanjay Choudhary <sanjaytech6375@gmail.com>
2026-04-06 21:23:04 +0530

    fix api url bug in post services page

--- src/services/server/post.service.ts
+++ src/services/server/post.service.ts
@@ -14,7 +14,7 @@ export const getPosts = cache(async (params?: ListPostsParams) => {
     filteredParams as Record<string, string>,
   ).toString()
 
-  const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/api/posts?${query}`, {
+  const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/posts?${query}`, {
     cache: 'no-store',
     next: {
       // revalidate: 60,
@@ -35,7 +35,7 @@ export const getPosts = cache(async (params?: ListPostsParams) => {
 })
 
 export const getPostBySlug = cache(async (slug: string) => {
-  const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/api/posts/${slug}`, {
+  const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/posts/${slug}`, {
     cache: 'no-store',
     next: {
       // revalidate: 60,
