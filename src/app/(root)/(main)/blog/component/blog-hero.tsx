037cf0b9d51c61a40259b5c136f6e2919820deff
Sanjay Choudhary <sanjaytech6375@gmail.com>
2026-04-22 18:30:59 +0530

    fix bugs in env

--- src/app/(root)/(main)/blog/component/blog-hero.tsx
+++ src/app/(root)/(main)/blog/component/blog-hero.tsx
@@ -11,9 +11,6 @@ interface Props {
 
 export function BlogHero({ post }: Props) {
   const stats = readingTime(post.content || '')
-
-  console.log(post, 'post')
-
   return (
     <div className="mb-12">
       <div className="mb-6 flex items-center justify-between">
